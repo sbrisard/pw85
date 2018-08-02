ffe4ee62933574128652a4fbc17c078e0d58a145
Sébastien Brisard <sebastien.brisard@m4x.org>
2018-08-02 09:00:00 +0200

    Remove classifiers

--- src/python/setup.py
+++ src/python/setup.py
@@ -4,12 +4,4 @@ setup(
     name='pw85',
     py_modules=['pw85'],
     license='BSD-3',
-    classifiers=['Development Status :: 4 - Beta',
-                 'Intended Audience :: Developers',
-                 'Intended Audience :: Science/Research',
-                 'License :: OSI Approved :: BSD License',
-                 'Operating System :: OS Independent',
-                 'Topic :: Software Development :: Build Tools',
-                 'Programming Language :: Python :: 3',
-                 'Topic :: Scientific/Engineering'],
 )
