e468cc321bb0c6cbdac7f9b64d0fd97ce03d648b
Sébastien Brisard <sebastien.brisard@m4x.org>
2018-08-02 08:50:56 +0200

    Define python module

---
+++ src/python/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='pw85',
+    py_modules=['pw85'],
+    license='BSD-3',
+    classifiers=['Development Status :: 4 - Beta',
+                 'Intended Audience :: Developers',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Topic :: Software Development :: Build Tools',
+                 'Programming Language :: Python :: 3',
+                 'Topic :: Scientific/Engineering'],
+)
