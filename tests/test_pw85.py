c8847fb9f277eda1b4b30e0e0fc7d5073cd8c453
Sébastien Brisard <sebastien.brisard@m4x.org>
2018-08-08 08:41:35 +0200

    Test of pw85_spheroid

--- tests/test_pw85.py
+++ tests/test_pw85.py
@@ -1,28 +1,53 @@
+import itertools
+
 import numpy as np
+import pytest
 import scipy.linalg
 
 import pw85
+
+from numpy.testing import assert_allclose
 
 
 def generate_directions(num_theta, num_phi):
     cos_theta = np.linspace(-1.0, 1.0, num=num_theta)
     sin_theta = np.sqrt(1.0-cos_theta**2)
-    phi = np.linspace(0., 2.*np.pi, num=num_phi, endpoint=False)
-    out = np.empty((num_theta, num_phi, 3), dtype=np._float64)
-    out[:, :, 0] = sin_theta*np.cos(phi)
-    out[:, :, 1] = sin_theta*np.sin(phi)
-    out[:, :, 2] = cos_theta
+    out = []
+    for cos_theta in np.linspace(-1.0, 1.0, num=num_theta):
+        sin_theta = np.sqrt(1.0-cos_theta**2)
+        for phi in np.linspace(0., 2.*np.pi, num=num_phi, endpoint=False):
+            out.append((sin_theta*np.cos(phi), sin_theta*np.sin(phi), cos_theta))
     return out
 
 
-def test_spheroid():
-    n = np.array([0., 0., 1.], dtype=np.float64)
-    q = pw85.spheroid(1.0, 0.1, n, None)
-    q = np.asarray(q)
-    q_mat = np.empty((3, 3), dtype=np.float64)
+RADII = [0.0199, 1.999, 9.999]
+DIRECTIONS = generate_directions(7, 7)
+BOOLEANS = [False, True]
+
+
+@pytest.mark.parametrize('a, c, n, in_place',
+                         itertools.product(RADII, RADII, DIRECTIONS, BOOLEANS))
+def test_spheroid(a, c, n, in_place, rtol=1E-7, atol=0):
+    inv_a2 = 1.0/a**2
+    inv_c2 = 1.0/c**2
+    n = np.asarray(n)
+    out = np.empty((6,), dtype=np.float64) if in_place else None
+    q = pw85.spheroid(a, c, n, out)
+    if in_place:
+        assert q is out
+    q_mat = np.zeros((3, 3), dtype=np.float64)
     # scipy.linalg.eigh only needs the lower-part of the matrix to be filled
     q_mat[:, 0] = q[:3]
     q_mat[1:, 1] = q[3:5]
     q_mat[2, 2] = q[5]
-    w, v = scipy.linalg.eigh(q_mat)
-    print(w)
+    w_act, v_act = scipy.linalg.eigh(q_mat)
+    if inv_a2 <= inv_c2:
+        w_exp = (inv_a2, inv_a2, inv_c2)
+        n_act = v_act[:, -1]
+    else:
+        w_exp = (inv_c2, inv_a2, inv_a2)
+        n_act = v_act[:, 0]
+    assert_allclose(w_act, w_exp, rtol, atol)
+    if a != c:
+        s = np.sign(np.dot(n_act, n))
+        assert_allclose(s*n_act, n)
