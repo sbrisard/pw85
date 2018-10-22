22c6f7b54881cb565d22dc1e8874a645d625087f
Sébastien Brisard <sebastien.brisard@m4x.org>
2018-10-22 11:00:25 +0200

    Formatting

--- pw85/pw85.c
+++ pw85/pw85.c
@@ -20,7 +20,7 @@ __declspec(dllexport) double pw85__det_sym(double a[PW85_SYM])
 }
 
 __declspec(dllexport) double pw85__xT_adjA_x(double x[PW85_DIM],
-					     double a[PW85_SYM])
+                                             double a[PW85_SYM])
 {
     return (x[0] * x[0] * (a[3] * a[5] - a[4] * a[4]) + x[1] * x[1] * (a[0] * a[5] - a[2] * a[2]) + x[2] * x[2] * (a[0] * a[3] - a[1] * a[1]) + 2. * (x[0] * x[1] * (a[2] * a[4] - a[1] * a[5]) + x[0] * x[2] * (a[1] * a[4] - a[2] * a[3]) + x[1] * x[2] * (a[1] * a[2] - a[0] * a[4])));
 }
@@ -44,7 +44,8 @@ __declspec(dllexport) void pw85_spheroid(double a, double c, double *n,
     q[PW85_XY] = nx * ny * c2_minus_a2;
 }
 
-__declspec(dllexport) void pw85_rT_adjQ_r_as_poly(double* r, double* q1, double* q2, double* q3, double* a) {
+__declspec(dllexport) void pw85_rT_adjQ_r_as_poly(double *r, double *q1, double *q2, double *q3, double *a)
+{
     const double a_zero = pw85__xT_adjA_x(r, q1);
     const double a_one = pw85__xT_adjA_x(r, q2);
     const double a_minus_one = pw85__xT_adjA_x(r, q3);
@@ -53,7 +54,8 @@ __declspec(dllexport) void pw85_rT_adjQ_r_as_poly(double* r, double* q1, double*
     a[1] = 0.5 * (a_one - a_minus_one);
 }
 
-__declspec(dllexport) void pw85_detQ_as_poly(double* q1, double* q2, double* q3, double* q4, double* b) {
+__declspec(dllexport) void pw85_detQ_as_poly(double *q1, double *q2, double *q3, double *q4, double *b)
+{
     const double b_zero = pw85__det_sym(q1);
     const double b_one = pw85__det_sym(q2);
     /* Compute det[(1-x)*q1+x*q2] for x = -1. */
@@ -66,12 +68,14 @@ __declspec(dllexport) void pw85_detQ_as_poly(double* q1, double* q2, double* q3,
     b[3] = (-8. * b_one_half + 6. * b_zero + 3. * b_one - b_minus_one) / 3.;
 }
 
-__declspec(dllexport) double pw85_contact_function(double *r, double *q1, double *q2, double* out) {
-    double q3[PW85_SYM];  /* q3 = 2*q1-q2. */
-    double q4[PW85_SYM];  /* q4 = (q1+q2)/2. */
-    for (int i = 0; i < PW85_SYM; i++) {
-	    q3[i] = 2. * q1[i] - q2[i];
-	    q4[i] = 0.5*(q1[i]+q2[i]);
+__declspec(dllexport) double pw85_contact_function(double *r, double *q1, double *q2, double *out)
+{
+    double q3[PW85_SYM]; /* q3 = 2*q1-q2. */
+    double q4[PW85_SYM]; /* q4 = (q1+q2)/2. */
+    for (int i = 0; i < PW85_SYM; i++)
+    {
+        q3[i] = 2. * q1[i] - q2[i];
+        q4[i] = 0.5 * (q1[i] + q2[i]);
     }
 
     double a[3];
@@ -92,18 +96,31 @@ __declspec(dllexport) double pw85_contact_function(double *r, double *q1, double
     double xr = 1.;
     double yr = c0 + c1 + c2 + c3 + c4 + c5 + c6;
     double x, y;
-    while (fabs(xr - xl) > 1E-12) {
+    while (fabs(xr - xl) > 1E-12)
+    {
         x = 0.5 * (xl + xr);
         y = c0 + x * (c1 + x * (c2 + x * (c3 + x * (c4 + x * (c5 + x * c6)))));
-        if (y == 0.0) {
+        if (y == 0.0)
+        {
             break;
-        } else {
-            if (yl * y < 0) { xr = x; yr = y; }
-            else { xl = x; yl = y; }
+        }
+        else
+        {
+            if (yl * y < 0)
+            {
+                xr = x;
+                yr = y;
+            }
+            else
+            {
+                xl = x;
+                yl = y;
+            }
         }
     }
     y = x * (1. - x) * (a[0] + x * (a[1] + x * a[2])) / (b[0] + x * (b[1] + x * (b[2] + x * b[3])));
-    if (out != NULL) {
+    if (out != NULL)
+    {
         out[0] = y;
         out[1] = x;
     }
