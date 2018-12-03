e3854ae42abc29eef3393488530f54ad4b39c64a
Sébastien Brisard <sebastien.brisard@m4x.org>
2018-12-03 10:31:21 +0100

    Typos

--- pw85/pw85.c
+++ pw85/pw85.c
@@ -23,7 +23,7 @@ __declspec(dllexport) void pw85__rT_adjQ_r_as_poly(double r[PW85_DIM],
 						   double q1[PW85_SYM],
 						   double q2[PW85_SYM],
 						   double q3[PW85_SYM],
-						   double a[PW85_DIM-1])
+						   double a[PW85_DIM])
 {
     const double a_zero = pw85__xT_adjA_x(r, q1);
     const double a_one = pw85__xT_adjA_x(r, q2);
@@ -37,7 +37,7 @@ __declspec(dllexport) void pw85__detQ_as_poly(double q1[PW85_SYM],
 					      double q2[PW85_SYM],
 					      double q3[PW85_SYM],
 					      double q4[PW85_SYM],
-					      double b[PW85_DIM])
+					      double b[PW85_DIM+1])
 {
     const double b_zero = pw85__det_sym(q1);
     const double b_one = pw85__det_sym(q2);
