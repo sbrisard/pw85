0d6bc83efbe0e01411eaa92dc682506f44dd5d06
Sébastien Brisard <sebastien.brisard@m4x.org>
2018-12-05 08:01:37 +0100

    Docstrings

--- pypw85/__init__.py
+++ pypw85/__init__.py
@@ -23,45 +23,57 @@ else:
 cpw85.pw85__det_sym.argtypes = [c_double_p]
 cpw85.pw85__det_sym.restype = c_double
 
-
-def _det_sym(a):
-    return cpw85.pw85__det_sym(a.ctypes.data_as(c_double_p))
-
-
 cpw85.pw85__xT_adjA_x.argtypes = [c_double_p, c_double_p]
 cpw85.pw85__xT_adjA_x.restype = c_double
-
-
-def _xT_adjA_x(x, a):
-    return cpw85.pw85__xT_adjA_x(x.ctypes.data_as(c_double_p),
-                                 a.ctypes.data_as(c_double_p))
-
 
 cpw85.pw85_spheroid.argtypes = [c_double, c_double, c_double_p, c_double_p]
 cpw85.pw85_spheroid.restype = None
 
-
-def spheroid(a, c, n, q=None):
-    """Return the quadratic form associated to a spheroid.
-
-    The spheroid is defined by its equatorial radius `a`, its polar
-    radius `c` and the direction of its axis of revolution, `n`
-    (array-like of length 3).
-
-    If `q` is not ``None``, then it must be an array of length 6. It
-    is modified in place.
-    """
-    if q is None:
-        q = np.empty((6,), dtype=np.float64, order='C')
-    # TODO Check input
-    cpw85.pw85_spheroid(a, c,
-                        n.ctypes.data_as(c_double_p),
-                        q.ctypes.data_as(c_double_p))
-    return q
-
-
 cpw85.pw85__rT_adjQ_r_as_poly.argtypes = 5*[c_double_p]
 cpw85.pw85__rT_adjQ_r_as_poly.restype = None
+
+cpw85.pw85__detQ_as_poly.argtypes = 5*[c_double_p]
+cpw85.pw85__detQ_as_poly.restype = None
+
+cpw85.pw85_contact_function.argtypes = 4*[c_double_p]
+cpw85.pw85_contact_function.restype = c_double
+
+
+def _det_sym(a):
+    """Return det(A) (``A``: 3×3, symmetric matrix).
+
+    The symmetric, 3×3 matrix ``A`` is specified trough the array-like
+    list `a` of the six coefficients of its triangular upper part,
+    listed in row-major order::
+
+          ⎡ a[0] a[1] a[2] ⎤
+      A = ⎢      a[3] a[4] ⎥.
+          ⎣ sym.      a[5] ⎦
+
+    """
+    return cpw85.pw85__det_sym(a.ctypes.data_as(c_double_p))
+
+
+def _xT_adjA_x(x, a):
+    """Return ``xᵀ⋅adj(A)⋅x`` (``A``: 3×3, symmetric matrix; ``x``: 3×1  vector).
+
+    The column vector ``x`` is specified through the array-like list
+    of its three coefficients::
+
+          ⎡ x[0] ⎤
+      x = ⎢ x[1] ⎥.
+          ⎣ x[2] ⎦
+
+    The symmetric, 3×3 matrix ``A`` is specified trough the array-like
+    list `a` of the six coefficients of its triangular upper part,
+    listed in row-major order::
+
+          ⎡ a[0] a[1] a[2] ⎤
+      A = ⎢      a[3] a[4] ⎥.
+          ⎣ sym.      a[5] ⎦
+    """
+    return cpw85.pw85__xT_adjA_x(x.ctypes.data_as(c_double_p),
+                                 a.ctypes.data_as(c_double_p))
 
 
 def _rT_adjQ_r_as_poly(r, q1, q2, q3=None, a=None):
@@ -72,10 +84,6 @@ def _rT_adjQ_r_as_poly(r, q1, q2, q3=None, a=None):
     args = [arg.ctypes.data_as(c_double_p) for arg in (r, q1, q2, q3, a)]
     cpw85.pw85__rT_adjQ_r_as_poly(*args)
     return a
-
-
-cpw85.pw85__detQ_as_poly.argtypes = 5*[c_double_p]
-cpw85.pw85__detQ_as_poly.restype = None
 
 
 def _detQ_as_poly(q1, q2, q3=None, q4=None, b=None):
@@ -90,8 +98,22 @@ def _detQ_as_poly(q1, q2, q3=None, q4=None, b=None):
     return b
 
 
-cpw85.pw85_contact_function.argtypes = 4*[c_double_p]
-cpw85.pw85_contact_function.restype = c_double
+def spheroid(a, c, n, q=None):
+    """Return the quadratic form associated to a spheroid.
+
+    The spheroid is defined by its equatorial radius `a`, its polar
+    radius `c` and the direction of its axis of revolution, `n`
+    (array-like of length 3).
+
+    If `q` is not ``None``, then it must be an array of length 6. It
+    is modified in place.
+    """
+    if q is None:
+        q = np.empty((6,), dtype=np.float64, order='C')
+    cpw85.pw85_spheroid(a, c,
+                        n.ctypes.data_as(c_double_p),
+                        q.ctypes.data_as(c_double_p))
+    return q
 
 
 def contact_function(r, q1, q2, out=None):
