75c2db0d5e6f30df6c6ae7fa86001dd3309884f1
Sébastien Brisard <sebastien.brisard@m4x.org>
2019-02-26 12:22:39 +0100

    Configure full path to native library

--- wrappers/python-ctypes/pypw85/lowlevel.py
+++ wrappers/python-ctypes/pypw85/lowlevel.py
@@ -1,20 +1,26 @@
 """Low-level interface to the PW85 library."""
 
+import configparser
 import ctypes
+import pathlib
 
 from ctypes import c_double
 
+
+def init_signature(func, argtypes, restype=None):
+    func.argtypes = argtypes
+    func.restype = restype
+
+
 c_double_p = ctypes.POINTER(c_double)
 
-# TODO: wrap in a function
-# path = os.path.expanduser('~/.pw85rc')
-# if os.path.isfile(path):
-#     cfg = configparser.ConfigParser()
-#     cfg.read(path)
-#     cpw85 = ctypes.cdll.LoadLibrary(cfg['Native']['LibraryPath'])
-# else:
-#     # TODO: improve error message
-#     raise RuntimeError("Configuration file not found.")
+path = pathlib.Path.home()/'pw85.ini'
+if path.is_file():
+    cfg = configparser.ConfigParser()
+    cfg.read(path)
+    cpw85 = ctypes.cdll.LoadLibrary(cfg['pw85']['FullPath'])
+else:
+    raise RuntimeError('Cannot file configuration file: {}'.format(path))
 
 cpw85 = ctypes.cdll.LoadLibrary('libpw85')
 
@@ -25,11 +31,6 @@ _det_sym = cpw85.pw85__det_sym
 _xT_adjA_x = cpw85.pw85__xT_adjA_x
 _detQ_as_poly = cpw85.pw85__detQ_as_poly
 _rT_adjQ_r_as_poly = cpw85.pw85__rT_adjQ_r_as_poly
-
-
-def init_signature(func, argtypes, restype=None):
-    func.argtypes = argtypes
-    func.restype = restype
 
 init_signature(spheroid, [c_double, c_double, c_double_p, c_double_p])
 init_signature(contact_function, 4*[c_double_p], c_double)
