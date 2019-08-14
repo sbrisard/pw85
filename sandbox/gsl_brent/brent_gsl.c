33c3a2adcf3abe076e22365e74dccd70b0f254c1
Sébastien Brisard <sebastien.brisard@m4x.org>
2019-08-14 09:04:17 +0200

    Brent algorithm (GSL impl.)

--- sandbox/gsl_brent/brent_gsl.c
+++ sandbox/gsl_brent/brent_gsl.c
@@ -1,15 +1,20 @@
+#include <float.h>
 #include <math.h>
 #include <stdio.h>
 
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_min.h>
+
 #include <pw85.h>
 
-int main() {
+double f(double x, void *params) {
   const double golden_ratio = (1. + sqrt(5.)) / 2.;
   const double norm = sqrt(1 + golden_ratio * golden_ratio);
   const double u_abs = 1. / norm;
   const double v_abs = golden_ratio / norm;
 
-  double r[] = {0., u_abs, v_abs};
+  double r12[] = {0., u_abs, v_abs};
   double n1[] = {0., u_abs, v_abs};
   double n2[] = {0., u_abs, v_abs};
   const double a1 = 1.999;
@@ -22,12 +27,41 @@ int main() {
   pw85_spheroid(a1, c1, n1, q1);
   pw85_spheroid(a2, c2, n2, q2);
 
-  for (size_t i = 0; i < PW85_SYM; i++)
-    printf("%g, ", q1[i]);
-  printf("\n");
-  for (size_t i = 0; i < PW85_SYM; i++)
-    printf("%g, ", q2[i]);
-  printf("\n");
+  return -pw85_f(x, r12, q1, q2, NULL);
+}
 
-  return 0;
+int main() {
+  int status, iter = 0, max_iter = 100;
+  double a = 0.;
+  double b = 1.;
+  double m = 0.5;
+  double fm = f(m, NULL);
+
+  const gsl_function F = {.function = &f, .params = NULL};
+  gsl_min_fminimizer *s = gsl_min_fminimizer_alloc(gsl_min_fminimizer_brent);
+  gsl_min_fminimizer_set(s, &F, m, a, b);
+
+  printf("using %s method\n", gsl_min_fminimizer_name(s));
+  printf("%5s %9s [%9s, %9s] %9s %10s\n", "iter", "value", "lower", "upper",
+         "min", "err(est)");
+  printf("%5d %.7f [%.7f, %.7f] %.7f %+.7f\n", iter, fm, a, b, m, b - a);
+
+  do {
+    iter++;
+    status = gsl_min_fminimizer_iterate(s);
+
+    m = gsl_min_fminimizer_x_minimum(s);
+    a = gsl_min_fminimizer_x_lower(s);
+    b = gsl_min_fminimizer_x_upper(s);
+    fm = gsl_min_fminimizer_f_minimum(s);
+
+    status = gsl_min_test_interval(a, b, 1e-8, 0.0);
+
+    if (status == GSL_SUCCESS)
+      printf("Converged:\n");
+
+    printf("%5d %.7f [%.7f, %.7f] %.7f %.7f\n", iter, fm, a, b, m, b - a);
+  } while (status == GSL_CONTINUE && iter < max_iter);
+
+  gsl_min_fminimizer_free(s);
 }
