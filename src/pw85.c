8b6625ec479a36125771cbf2cda1a12c05d221fe
Sébastien Brisard <sebastien.brisard@m4x.org>
2019-08-23 08:18:27 +0200

    Add some `const` qualifiers

--- src/pw85.c
+++ src/pw85.c
@@ -11,12 +11,12 @@
 /* ================= */
 /* These functions are exported for the sake of testing only. */
 
-double pw85__det_sym(double a[PW85_SYM]) {
+double pw85__det_sym(double const a[PW85_SYM]) {
   return a[0] * a[3] * a[5] + 2 * a[1] * a[2] * a[4] - a[0] * a[4] * a[4] -
          a[3] * a[2] * a[2] - a[5] * a[1] * a[1];
 }
 
-double pw85__xT_adjA_x(double x[PW85_DIM], double a[PW85_SYM]) {
+double pw85__xT_adjA_x(double const x[PW85_DIM], double const a[PW85_SYM]) {
   return (x[0] * x[0] * (a[3] * a[5] - a[4] * a[4]) +
           x[1] * x[1] * (a[0] * a[5] - a[2] * a[2]) +
           x[2] * x[2] * (a[0] * a[3] - a[1] * a[1]) +
@@ -25,33 +25,34 @@ double pw85__xT_adjA_x(double x[PW85_DIM], double a[PW85_SYM]) {
                 x[1] * x[2] * (a[1] * a[2] - a[0] * a[4])));
 }
 
-void pw85__rT_adjQ_r_as_poly(double r[PW85_DIM], double q1[PW85_SYM],
-                             double q2[PW85_SYM], double q3[PW85_SYM],
-                             double a[PW85_DIM]) {
-  const double a_zero = pw85__xT_adjA_x(r, q1);
-  const double a_one = pw85__xT_adjA_x(r, q2);
-  const double a_minus_one = pw85__xT_adjA_x(r, q3);
+void pw85__rT_adjQ_r_as_poly(double const r[PW85_DIM],
+                             double const q1[PW85_SYM],
+                             double const q2[PW85_SYM],
+                             double const q3[PW85_SYM], double a[PW85_DIM]) {
+  double const a_zero = pw85__xT_adjA_x(r, q1);
+  double const a_one = pw85__xT_adjA_x(r, q2);
+  double const a_minus_one = pw85__xT_adjA_x(r, q3);
   a[0] = a_zero;
   a[2] = 0.5 * (a_one + a_minus_one) - a_zero;
   a[1] = 0.5 * (a_one - a_minus_one);
 }
 
-void pw85__detQ_as_poly(double q1[PW85_SYM], double q2[PW85_SYM],
-                        double q3[PW85_SYM], double q4[PW85_SYM],
+void pw85__detQ_as_poly(double const q1[PW85_SYM], double const q2[PW85_SYM],
+                        double const q3[PW85_SYM], double const q4[PW85_SYM],
                         double b[PW85_DIM + 1]) {
-  const double b_zero = pw85__det_sym(q1);
-  const double b_one = pw85__det_sym(q2);
+  double const b_zero = pw85__det_sym(q1);
+  double const b_one = pw85__det_sym(q2);
   /* Compute det[(1-x)*q1+x*q2] for x = -1. */
-  const double b_minus_one = pw85__det_sym(q3);
+  double const b_minus_one = pw85__det_sym(q3);
   /* Compute det[(1-x)*q1+x*q2] for x = 1/2. */
-  const double b_one_half = pw85__det_sym(q4);
+  double const b_one_half = pw85__det_sym(q4);
   b[0] = b_zero;
   b[2] = 0.5 * (b_one + b_minus_one) - b_zero;
   b[1] = (8. * b_one_half - 6. * b_zero - 1.5 * b_one - 0.5 * b_minus_one) / 3.;
   b[3] = (-8. * b_one_half + 6. * b_zero + 3. * b_one - b_minus_one) / 3.;
 }
 
-void pw85__cholesky_decomp(double a[PW85_SYM], double l[PW85_SYM]) {
+void pw85__cholesky_decomp(double const a[PW85_SYM], double l[PW85_SYM]) {
   l[0] = sqrt(a[0]);
   l[1] = a[1] / l[0];
   l[2] = a[2] / l[0];
@@ -60,12 +61,12 @@ void pw85__cholesky_decomp(double a[PW85_SYM], double l[PW85_SYM]) {
   l[5] = sqrt(a[5] - l[2] * l[2] - l[4] * l[4]);
 }
 
-void pw85__cholesky_solve(double l[PW85_SYM], double b[PW85_DIM],
+void pw85__cholesky_solve(double const l[PW85_SYM], double const b[PW85_DIM],
                           double x[PW85_DIM]) {
   /* Solve L.y = b */
-  const double y0 = b[0] / l[0];
-  const double y1 = (b[1] - l[1] * y0) / l[3];
-  const double y2 = (b[2] - l[2] * y0 - l[4] * y1) / l[5];
+  double const y0 = b[0] / l[0];
+  double const y1 = (b[1] - l[1] * y0) / l[3];
+  double const y2 = (b[2] - l[2] * y0 - l[4] * y1) / l[5];
 
   /* Solve L^T.x = y */
   x[2] = y2 / l[5];
@@ -76,12 +77,13 @@ void pw85__cholesky_solve(double l[PW85_SYM], double b[PW85_DIM],
 /* Public API */
 /* ========== */
 
-void pw85_spheroid(double a, double c, double n[PW85_DIM], double q[PW85_SYM]) {
-  const double a2 = a * a;
-  const double c2_minus_a2 = c * c - a2;
-  const double nx = n[0];
-  const double ny = n[1];
-  const double nz = n[2];
+void pw85_spheroid(double a, double c, double const n[PW85_DIM],
+                   double q[PW85_SYM]) {
+  double const a2 = a * a;
+  double const c2_minus_a2 = c * c - a2;
+  double const nx = n[0];
+  double const ny = n[1];
+  double const nz = n[2];
   q[0] = nx * nx * c2_minus_a2 + a2;
   q[3] = ny * ny * c2_minus_a2 + a2;
   q[5] = nz * nz * c2_minus_a2 + a2;
@@ -90,10 +92,10 @@ void pw85_spheroid(double a, double c, double n[PW85_DIM], double q[PW85_SYM]) {
   q[1] = nx * ny * c2_minus_a2;
 }
 
-double pw85_f_neg(double lambda, double *params) {
-  double *r12 = params;
-  double *q1_i = params + PW85_DIM;
-  double *q2_i = q1_i + PW85_SYM;
+double pw85_f_neg(double lambda, double const *params) {
+  double const *r12 = params;
+  double const *q1_i = params + PW85_DIM;
+  double const *q2_i = q1_i + PW85_SYM;
   double q[PW85_SYM];
   double *q_i = q;
   double q12[PW85_SYM];
@@ -106,7 +108,8 @@ double pw85_f_neg(double lambda, double *params) {
   pw85__cholesky_decomp(q, l);
   double s[PW85_DIM];
   pw85__cholesky_solve(l, r12, s);
-  double *r_i = r12, *s_i = s;
+  double const *r_i = r12;
+  double *s_i = s;
   double rs = 0.;
   for (size_t i = 0; i < PW85_DIM; i++, r_i++, s_i++) {
     rs += (*r_i) * (*s_i);
@@ -114,11 +117,12 @@ double pw85_f_neg(double lambda, double *params) {
   return -lambda * (1. - lambda) * rs;
 }
 
-double pw85_contact_function(double r12[PW85_DIM], double q1[PW85_SYM],
-                             double q2[PW85_SYM], double *out) {
+double pw85_contact_function(double const r12[PW85_DIM],
+                             double const q1[PW85_SYM],
+                             double const q2[PW85_SYM], double *out) {
   const size_t max_iter = 100;
 
-  const double params[] = {r12[0], r12[1], r12[2], q1[0], q1[1],
+  double const params[] = {r12[0], r12[1], r12[2], q1[0], q1[1],
                            q1[2],  q1[3],  q1[4],  q1[5], q2[0],
                            q2[1],  q2[2],  q2[3],  q2[4], q2[5]};
 
@@ -129,13 +133,13 @@ double pw85_contact_function(double r12[PW85_DIM], double q1[PW85_SYM],
 
   for (size_t iter = 0; iter < max_iter; iter++) {
     gsl_min_fminimizer_iterate(s);
-    const double a = gsl_min_fminimizer_x_lower(s);
-    const double b = gsl_min_fminimizer_x_upper(s);
+    double const a = gsl_min_fminimizer_x_lower(s);
+    double const b = gsl_min_fminimizer_x_upper(s);
 
     if (gsl_min_test_interval(a, b, 1e-8, 0.0) == GSL_SUCCESS) break;
   }
 
-  const double ret = -gsl_min_fminimizer_f_minimum(s);
+  double const ret = -gsl_min_fminimizer_f_minimum(s);
   if (out) {
     out[0] = ret;
     out[1] = gsl_min_fminimizer_x_minimum(s);
@@ -144,10 +148,14 @@ double pw85_contact_function(double r12[PW85_DIM], double q1[PW85_SYM],
   return ret;
 }
 
-double pw85_f1(double lambda, double r12[PW85_DIM], double q1[PW85_SYM],
-               double q2[PW85_SYM], double *out) {
+double pw85_f1(double lambda, double const r12[PW85_DIM],
+               double const q1[PW85_SYM], double const q2[PW85_SYM],
+               double *out) {
   double q[PW85_SYM], q12[PW85_SYM];
-  double *q1_i = q1, *q2_i = q2, *q_i = q, *q12_i = q12;
+  double const *q1_i = q1;
+  double const *q2_i = q2;
+  double *q_i = q;
+  double *q12_i = q12;
   for (size_t i = 0; i < PW85_SYM; i++) {
     *q_i = (1 - lambda) * (*q1_i) + lambda * (*q2_i);
     *q12_i = (*q2_i) - (*q1_i);
@@ -167,7 +175,10 @@ double pw85_f1(double lambda, double r12[PW85_DIM], double q1[PW85_SYM],
   double v[PW85_DIM];
   pw85__cholesky_solve(l, u, v);
 
-  double *r_i = r12, *s_i = s, *u_i = u, *v_i = v;
+  double const *r_i = r12;
+  double *s_i = s;
+  double *u_i = u;
+  double *v_i = v;
   double rs = 0., su = 0., uv = 0.;
   for (size_t i = 0; i < PW85_DIM; i++) {
     rs += (*r_i) * (*s_i);
@@ -179,8 +190,8 @@ double pw85_f1(double lambda, double r12[PW85_DIM], double q1[PW85_SYM],
     ++v_i;
   }
 
-  const double aux1 = lambda * (1. - lambda);
-  const double aux2 = 1. - 2. * lambda;
+  double const aux1 = lambda * (1. - lambda);
+  double const aux2 = 1. - 2. * lambda;
   if (out) {
     out[0] = aux1 * rs;
     out[1] = aux2 * rs - aux1 * su;
@@ -191,8 +202,9 @@ double pw85_f1(double lambda, double r12[PW85_DIM], double q1[PW85_SYM],
   }
 }
 
-double pw85_contact_function1(double r12[PW85_DIM], double q1[PW85_SYM],
-                              double q2[PW85_SYM], double *out) {
+double pw85_contact_function1(double const r12[PW85_DIM],
+                              double const q1[PW85_SYM],
+                              double const q2[PW85_SYM], double *out) {
   double f[3];
   /* Lower-bound for the root. */
   double a = 0., f_prime_a;
@@ -230,8 +242,9 @@ double pw85_contact_function1(double r12[PW85_DIM], double q1[PW85_SYM],
   return f[0];
 }
 
-double pw85_f2(double lambda, double r12[PW85_DIM], double q1[PW85_SYM],
-               double q2[PW85_SYM], double *out) {
+double pw85_f2(double lambda, double const r12[PW85_DIM],
+               double const q1[PW85_SYM], double const q2[PW85_SYM],
+               double *out) {
   double q3[PW85_SYM]; /* q3 = 2*q1-q2. */
   double q4[PW85_SYM]; /* q4 = (q1+q2)/2. */
   for (int i = 0; i < PW85_SYM; i++) {
@@ -245,25 +258,26 @@ double pw85_f2(double lambda, double r12[PW85_DIM], double q1[PW85_SYM],
   double b[4];
   pw85__detQ_as_poly(q1, q2, q3, q4, b);
 
-  const double c0 = a[0] * b[0];
-  const double c1 = 2. * (a[1] - a[0]) * b[0];
-  const double c2 =
+  double const c0 = a[0] * b[0];
+  double const c1 = 2. * (a[1] - a[0]) * b[0];
+  double const c2 =
       -a[0] * (b[1] + b[2]) + 3. * b[0] * (a[2] - a[1]) + a[1] * b[1];
-  const double c3 =
+  double const c3 =
       2. * (b[1] * (a[2] - a[1]) - a[0] * b[3]) - 4. * a[2] * b[0];
-  const double c4 =
+  double const c4 =
       (a[0] - a[1]) * b[3] + (a[2] - a[1]) * b[2] - 3. * a[2] * b[1];
-  const double c5 = -2. * a[2] * b[2];
-  const double c6 = -a[2] * b[3];
-  const double y = lambda * (1. - lambda) *
+  double const c5 = -2. * a[2] * b[2];
+  double const c6 = -a[2] * b[3];
+  double const y = lambda * (1. - lambda) *
                    (a[0] + lambda * (a[1] + lambda * a[2])) /
                    (b[0] + lambda * (b[1] + lambda * (b[2] + lambda * b[3])));
   if (out) out[0] = y;
   return y;
 }
 
-double pw85_contact_function2(double r12[PW85_DIM], double q1[PW85_SYM],
-                              double q2[PW85_SYM], double *out) {
+double pw85_contact_function2(double const r12[PW85_DIM],
+                              double const q1[PW85_SYM],
+                              double const q2[PW85_SYM], double *out) {
   double q3[PW85_SYM]; /* q3 = 2*q1-q2. */
   double q4[PW85_SYM]; /* q4 = (q1+q2)/2. */
   for (int i = 0; i < PW85_SYM; i++) {
@@ -281,16 +295,16 @@ double pw85_contact_function2(double r12[PW85_DIM], double q1[PW85_SYM],
   printf("b = \n");
   for (size_t i = 0; i < 4; i++) printf("%1.15f\n", b[i]);
 
-  const double c0 = a[0] * b[0];
-  const double c1 = 2. * (a[1] - a[0]) * b[0];
-  const double c2 =
+  double const c0 = a[0] * b[0];
+  double const c1 = 2. * (a[1] - a[0]) * b[0];
+  double const c2 =
       -a[0] * (b[1] + b[2]) + 3. * b[0] * (a[2] - a[1]) + a[1] * b[1];
-  const double c3 =
+  double const c3 =
       2. * (b[1] * (a[2] - a[1]) - a[0] * b[3]) - 4. * a[2] * b[0];
-  const double c4 =
+  double const c4 =
       (a[0] - a[1]) * b[3] + (a[2] - a[1]) * b[2] - 3. * a[2] * b[1];
-  const double c5 = -2. * a[2] * b[2];
-  const double c6 = -a[2] * b[3];
+  double const c5 = -2. * a[2] * b[2];
+  double const c6 = -a[2] * b[3];
   double xl = 0.;
   double yl = c0;
   double xr = 1.;
