82cff8bffea6b65f1e774a1f2f3d4d0b5df496f0
Sebastien Brisard <sebastien.brisard@m4x.org>
2019-09-07 06:35:16 +0200

    black

--- wrappers/python-ctypes/tests/test_pw85.py
+++ wrappers/python-ctypes/tests/test_pw85.py
@@ -17,19 +17,17 @@ def setup_module():
 
 def gen_directions():
     # Use some vertices of icosahedron
-    golden_ratio = (1.+np.sqrt(5.))/2.
-    norm = np.sqrt(1+golden_ratio**2)
-    u_abs = 1./norm
-    v_abs = golden_ratio/norm
+    golden_ratio = (1.0 + np.sqrt(5.0)) / 2.0
+    norm = np.sqrt(1 + golden_ratio ** 2)
+    u_abs = 1.0 / norm
+    v_abs = golden_ratio / norm
     # out = []
     # for u in (-u_abs, u_abs):
     #     for v in (-v_abs, v_abs):
     #         out += [np.array((0., u, v), dtype=np.float64),
     #                 np.array((v, 0., u), dtype=np.float64),
     #                 np.array((u, v, 0.), dtype=np.float64)]
-    out = [[0., u_abs, v_abs],
-           [v_abs, 0., u_abs],
-           [u_abs, v_abs, 0.]]
+    out = [[0.0, u_abs, v_abs], [v_abs, 0.0, u_abs], [u_abs, v_abs, 0.0]]
     return np.array(out, dtype=np.float64)
 
 
@@ -38,48 +36,49 @@ DIRECTIONS = gen_directions()
 
 
 def to_array_2d(a):
-    return np.array([[a[0], a[1], a[2]],
-                     [a[1], a[3], a[4]],
-                     [a[2], a[4], a[5]]])
+    return np.array([[a[0], a[1], a[2]], [a[1], a[3], a[4]], [a[2], a[4], a[5]]])
 
 
-@pytest.mark.parametrize('l', [[1., 2., 3., 4., 5., 6.],
-                               [1., -2., -3., 4., -5., 6.],
-                               [1e5, -2e-5, -3e-5, 4., -5e-5, 6.e-5]])
+@pytest.mark.parametrize(
+    "l",
+    [
+        [1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
+        [1.0, -2.0, -3.0, 4.0, -5.0, 6.0],
+        [1e5, -2e-5, -3e-5, 4.0, -5e-5, 6.0e-5],
+    ],
+)
 def test__cholesky_decomp(l, rtol=1e-15, atol=1e-15):
     l = np.asarray(l)
-    l_mat = np.array([[l[0], 0., 0.],
-                      [l[1], l[3], 0.],
-                      [l[2], l[4], l[5]]], dtype=np.float64)
+    l_mat = np.array(
+        [[l[0], 0.0, 0.0], [l[1], l[3], 0.0], [l[2], l[4], l[5]]], dtype=np.float64
+    )
     a_mat = l_mat.dot(l_mat.T)
-    a = a_mat[[0, 0, 0, 1, 1, 2],
-              [0, 1, 2, 1, 2, 2]]
-    assert_allclose(pypw85._cholesky_decomp(a), l,
-                    rtol=rtol, atol=atol)
+    a = a_mat[[0, 0, 0, 1, 1, 2], [0, 1, 2, 1, 2, 2]]
+    assert_allclose(pypw85._cholesky_decomp(a), l, rtol=rtol, atol=atol)
 
 
-@pytest.mark.parametrize('l', [[1., 2., 3., 4., 5., 6.],
-                               [1., -2., -3., 4., -5., 6.]])
-@pytest.mark.parametrize('x', [[1.2, -3.4, 5.7]])
+@pytest.mark.parametrize(
+    "l", [[1.0, 2.0, 3.0, 4.0, 5.0, 6.0], [1.0, -2.0, -3.0, 4.0, -5.0, 6.0]]
+)
+@pytest.mark.parametrize("x", [[1.2, -3.4, 5.7]])
 def test__cholesky_solve(l, x, rtol=3e-15, atol=3e-15):
     l = np.asarray(l)
     x = np.asarray(x)
-    l_mat = np.array([[l[0], 0., 0.],
-                      [l[1], l[3], 0.],
-                      [l[2], l[4], l[5]]], dtype=np.float64)
+    l_mat = np.array(
+        [[l[0], 0.0, 0.0], [l[1], l[3], 0.0], [l[2], l[4], l[5]]], dtype=np.float64
+    )
     a_mat = l_mat.dot(l_mat.T)
     b = a_mat.dot(x)
-    assert_allclose(pypw85._cholesky_solve(l, b), x,
-                    rtol=rtol, atol=atol)
+    assert_allclose(pypw85._cholesky_solve(l, b), x, rtol=rtol, atol=atol)
 
 
-@pytest.mark.parametrize('a', RADII)
-@pytest.mark.parametrize('c', RADII)
-@pytest.mark.parametrize('n', DIRECTIONS)
-@pytest.mark.parametrize('in_place', [False, True])
-def test_spheroid(a, c, n, in_place, rtol=1E-10, atol=1E-12):
-    a2 = a**2
-    c2 = c**2
+@pytest.mark.parametrize("a", RADII)
+@pytest.mark.parametrize("c", RADII)
+@pytest.mark.parametrize("n", DIRECTIONS)
+@pytest.mark.parametrize("in_place", [False, True])
+def test_spheroid(a, c, n, in_place, rtol=1e-10, atol=1e-12):
+    a2 = a ** 2
+    c2 = c ** 2
     n = np.asarray(n)
     out = np.empty((6,), dtype=np.float64) if in_place else None
     q = pypw85.spheroid(a, c, n, out)
@@ -100,10 +99,10 @@ def test_spheroid(a, c, n, in_place, rtol=1E-10, atol=1E-12):
     assert_allclose(w_act, w_exp, rtol, atol)
     if a != c:
         s = np.sign(np.dot(n_act, n))
-        assert_allclose(s*n_act, n, rtol, atol)
+        assert_allclose(s * n_act, n, rtol, atol)
 
 
-def _test_contact_function(r, a1, c1, n1, a2, c2, n2, rtol=1E-9, atol=1E-11):
+def _test_contact_function(r, a1, c1, n1, a2, c2, n2, rtol=1e-9, atol=1e-11):
     # Test contact function with full output (μ² and λ)
     q1 = pypw85.spheroid(a1, c1, n1)
     q2 = pypw85.spheroid(a2, c2, n2)
@@ -113,15 +112,15 @@ def _test_contact_function(r, a1, c1, n1, a2, c2, n2, rtol=1E-9, atol=1E-11):
     μ2, λ = out
 
     Q1, Q2 = to_array_2d(q1), to_array_2d(q2)
-    Q = (1-λ)*Q1 + λ*Q2
+    Q = (1 - λ) * Q1 + λ * Q2
 
     # Check that stationarity condition
     #     λQ₁⁻¹⋅[x₀(λ)-c₁] + (1-λ)Q₂⁻¹⋅[x₀(λ)-c₂] = 0
     # is satisfied.
     y = np.linalg.solve(Q, r)
-    c1_x0 = np.dot((1-λ)*Q1, y)
-    c2_x0 = -np.dot(λ*Q2, y)
-    r_actual = c1_x0-c2_x0
+    c1_x0 = np.dot((1 - λ) * Q1, y)
+    c2_x0 = -np.dot(λ * Q2, y)
+    r_actual = c1_x0 - c2_x0
     assert_allclose(r_actual, r, rtol, atol)
 
     # Check that point x0 belongs to both scaled ellipsoids
@@ -132,48 +131,50 @@ def _test_contact_function(r, a1, c1, n1, a2, c2, n2, rtol=1E-9, atol=1E-11):
     assert_allclose(μ2, pypw85.contact_function(r, q1, q2), rtol, atol)
 
 
-@pytest.mark.parametrize('r', DIRECTIONS)
-@pytest.mark.parametrize('a1', RADII)
-@pytest.mark.parametrize('c1', RADII)
-@pytest.mark.parametrize('n1', DIRECTIONS)
-@pytest.mark.parametrize('a2', RADII)
-@pytest.mark.parametrize('c2', RADII)
-@pytest.mark.parametrize('n2', DIRECTIONS)
+@pytest.mark.parametrize("r", DIRECTIONS)
+@pytest.mark.parametrize("a1", RADII)
+@pytest.mark.parametrize("c1", RADII)
+@pytest.mark.parametrize("n1", DIRECTIONS)
+@pytest.mark.parametrize("a2", RADII)
+@pytest.mark.parametrize("c2", RADII)
+@pytest.mark.parametrize("n2", DIRECTIONS)
 def test_contact_function_fixed_cc_distance(r, a1, c1, n1, a2, c2, n2):
     _test_contact_function(r, a1, c1, n1, a2, c2, n2)
 
 
-@pytest.mark.parametrize('r', DIRECTIONS[0, :]*RADII[:, None])
-@pytest.mark.parametrize('a1', RADII)
-@pytest.mark.parametrize('c1', RADII)
-@pytest.mark.parametrize('n1', [DIRECTIONS[1]])
-@pytest.mark.parametrize('a2', RADII)
-@pytest.mark.parametrize('c2', RADII)
-@pytest.mark.parametrize('n2', [DIRECTIONS[2]])
+@pytest.mark.parametrize("r", DIRECTIONS[0, :] * RADII[:, None])
+@pytest.mark.parametrize("a1", RADII)
+@pytest.mark.parametrize("c1", RADII)
+@pytest.mark.parametrize("n1", [DIRECTIONS[1]])
+@pytest.mark.parametrize("a2", RADII)
+@pytest.mark.parametrize("c2", RADII)
+@pytest.mark.parametrize("n2", [DIRECTIONS[2]])
 def test_contact_function_variable_cc_distance(r, a1, c1, n1, a2, c2, n2):
     _test_contact_function(r, a1, c1, n1, a2, c2, n2)
 
 
-@pytest.mark.parametrize('r12_dir', DIRECTIONS)
-@pytest.mark.parametrize('a1', RADII)
-@pytest.mark.parametrize('c1', RADII)
-@pytest.mark.parametrize('n1', DIRECTIONS)
-@pytest.mark.parametrize('a2', RADII)
-@pytest.mark.parametrize('c2', RADII)
-@pytest.mark.parametrize('n2', DIRECTIONS)
-def test_f(r12_dir, a1, c1, n1, a2, c2, n2,
-           num_lambdas=11, rtol=1e-10, atol=1e-10):
+@pytest.mark.parametrize("r12_dir", DIRECTIONS)
+@pytest.mark.parametrize("a1", RADII)
+@pytest.mark.parametrize("c1", RADII)
+@pytest.mark.parametrize("n1", DIRECTIONS)
+@pytest.mark.parametrize("a2", RADII)
+@pytest.mark.parametrize("c2", RADII)
+@pytest.mark.parametrize("n2", DIRECTIONS)
+def test_f(r12_dir, a1, c1, n1, a2, c2, n2, num_lambdas=11, rtol=1e-10, atol=1e-10):
     q1 = pypw85.spheroid(a1, c1, n1)
     q2 = pypw85.spheroid(a2, c2, n2)
     q1_mat = to_array_2d(q1)
     q2_mat = to_array_2d(q2)
     expected = []
     actual = []
-    for lambda_i in np.linspace(0., 1., num=num_lambdas):
-        q_mat = (1.-lambda_i)*q1_mat+lambda_i*q2_mat
-        exp_ref = (lambda_i*(1.-lambda_i)
-                   *np.dot(np.linalg.solve(q_mat, r12_dir), r12_dir))
+    for lambda_i in np.linspace(0.0, 1.0, num=num_lambdas):
+        q_mat = (1.0 - lambda_i) * q1_mat + lambda_i * q2_mat
+        exp_ref = (
+            lambda_i
+            * (1.0 - lambda_i)
+            * np.dot(np.linalg.solve(q_mat, r12_dir), r12_dir)
+        )
         for r12_norm_j in RADII:
-            expected.append(r12_norm_j**2*exp_ref)
-            actual.append(pypw85.f(lambda_i, r12_norm_j*r12_dir, q1, q2))
+            expected.append(r12_norm_j ** 2 * exp_ref)
+            actual.append(pypw85.f(lambda_i, r12_norm_j * r12_dir, q1, q2))
     assert_allclose(actual, expected, rtol=rtol, atol=atol)
--- wrappers/python-ctypes/tests/test_pw85_legacy.py
+++ wrappers/python-ctypes/tests/test_pw85_legacy.py
@@ -17,19 +17,17 @@ def setup_module():
 
 def gen_directions():
     # Use some vertices of icosahedron
-    golden_ratio = (1.+np.sqrt(5.))/2.
-    norm = np.sqrt(1+golden_ratio**2)
-    u_abs = 1./norm
-    v_abs = golden_ratio/norm
+    golden_ratio = (1.0 + np.sqrt(5.0)) / 2.0
+    norm = np.sqrt(1 + golden_ratio ** 2)
+    u_abs = 1.0 / norm
+    v_abs = golden_ratio / norm
     # out = []
     # for u in (-u_abs, u_abs):
     #     for v in (-v_abs, v_abs):
     #         out += [np.array((0., u, v), dtype=np.float64),
     #                 np.array((v, 0., u), dtype=np.float64),
     #                 np.array((u, v, 0.), dtype=np.float64)]
-    out = [[0., u_abs, v_abs],
-           [v_abs, 0., u_abs],
-           [u_abs, v_abs, 0.]]
+    out = [[0.0, u_abs, v_abs], [v_abs, 0.0, u_abs], [u_abs, v_abs, 0.0]]
     return np.array(out, dtype=np.float64)
 
 
@@ -38,36 +36,34 @@ DIRECTIONS = gen_directions()
 
 
 def to_array_2d(a):
-    return np.array([[a[0], a[1], a[2]],
-                     [a[1], a[3], a[4]],
-                     [a[2], a[4], a[5]]])
+    return np.array([[a[0], a[1], a[2]], [a[1], a[3], a[4]], [a[2], a[4], a[5]]])
 
 
-@pytest.mark.parametrize('a', np.random.rand(100, 6))
-def test__det_sym(a, rtol=1E-12, atol=1E-14):
+@pytest.mark.parametrize("a", np.random.rand(100, 6))
+def test__det_sym(a, rtol=1e-12, atol=1e-14):
 
     expected = np.linalg.det(to_array_2d(a))
     actual = pypw85._det_sym(a)
     assert_allclose(actual, expected, rtol, atol)
 
 
-@pytest.mark.parametrize('a1', RADII)
-@pytest.mark.parametrize('c1', RADII)
-@pytest.mark.parametrize('n1', DIRECTIONS)
-@pytest.mark.parametrize('a2', RADII)
-@pytest.mark.parametrize('c2', RADII)
-@pytest.mark.parametrize('n2', DIRECTIONS)
-def test__detQ_as_poly(a1, c1, n1, a2, c2, n2, rtol=1E-10, atol=1E-8):
+@pytest.mark.parametrize("a1", RADII)
+@pytest.mark.parametrize("c1", RADII)
+@pytest.mark.parametrize("n1", DIRECTIONS)
+@pytest.mark.parametrize("a2", RADII)
+@pytest.mark.parametrize("c2", RADII)
+@pytest.mark.parametrize("n2", DIRECTIONS)
+def test__detQ_as_poly(a1, c1, n1, a2, c2, n2, rtol=1e-10, atol=1e-8):
     q1 = pypw85.spheroid(a1, c1, n1)
     Q1 = to_array_2d(q1)
     q2 = pypw85.spheroid(a2, c2, n2)
     Q2 = to_array_2d(q2)
-    x = np.linspace(0., 1., num=11)
+    x = np.linspace(0.0, 1.0, num=11)
     b = np.poly1d(pypw85._detQ_as_poly(q1, q2)[::-1])
     actual = b(x)
 
     x = x[:, None, None]
-    expected = np.linalg.det((1.-x)*Q1+x*Q2)
+    expected = np.linalg.det((1.0 - x) * Q1 + x * Q2)
     assert_allclose(actual, expected, rtol, atol)
 
 
@@ -79,7 +75,7 @@ def minor(A, i, j):
     rows.remove(i)
     cols = list(range(n))
     cols.remove(j)
-    rows, cols = np.meshgrid(rows, cols, indexing='ij')
+    rows, cols = np.meshgrid(rows, cols, indexing="ij")
     return A[..., rows, cols]
 
 
@@ -87,46 +83,46 @@ def adjugate(A):
     adjA = np.empty_like(A)
     for i in range(3):
         for j in range(3):
-            adjA[..., i, j] = (-1)**(i+j)*np.linalg.det(minor(A, i, j))
+            adjA[..., i, j] = (-1) ** (i + j) * np.linalg.det(minor(A, i, j))
     return adjA
 
 
-@pytest.mark.parametrize('x', np.random.rand(5, 3))
-@pytest.mark.parametrize('a', np.random.rand(5, 6))
-def test__xT_adjA_x(x, a, rtol=1E-12, atol=1E-14):
+@pytest.mark.parametrize("x", np.random.rand(5, 3))
+@pytest.mark.parametrize("a", np.random.rand(5, 6))
+def test__xT_adjA_x(x, a, rtol=1e-12, atol=1e-14):
     actual = pypw85._xT_adjA_x(x, a)
     expected = np.dot(x, np.dot(adjugate(to_array_2d(a)), x))
     assert_allclose(actual, expected, rtol, atol)
 
 
-def _test__rT_adjQ_r_as_poly(r, a1, c1, n1, a2, c2, n2, rtol=1E-10, atol=1E-8):
+def _test__rT_adjQ_r_as_poly(r, a1, c1, n1, a2, c2, n2, rtol=1e-10, atol=1e-8):
     q1 = pypw85.spheroid(a1, c1, n1)
     q2 = pypw85.spheroid(a2, c2, n2)
-    x = np.linspace(0., 1., num=11)
+    x = np.linspace(0.0, 1.0, num=11)
     actual = np.poly1d(pypw85._rT_adjQ_r_as_poly(r, q1, q2)[::-1])(x)
     x = x[:, None, None]
-    Q = (1-x)*to_array_2d(q1)+x*to_array_2d(q2)
+    Q = (1 - x) * to_array_2d(q1) + x * to_array_2d(q2)
     expected = np.dot(np.dot(adjugate(Q), r), r)
     assert_allclose(actual, expected, rtol, atol)
 
 
-@pytest.mark.parametrize('r', DIRECTIONS)
-@pytest.mark.parametrize('a1', RADII)
-@pytest.mark.parametrize('c1', RADII)
-@pytest.mark.parametrize('n1', DIRECTIONS)
-@pytest.mark.parametrize('a2', RADII)
-@pytest.mark.parametrize('c2', RADII)
-@pytest.mark.parametrize('n2', DIRECTIONS)
+@pytest.mark.parametrize("r", DIRECTIONS)
+@pytest.mark.parametrize("a1", RADII)
+@pytest.mark.parametrize("c1", RADII)
+@pytest.mark.parametrize("n1", DIRECTIONS)
+@pytest.mark.parametrize("a2", RADII)
+@pytest.mark.parametrize("c2", RADII)
+@pytest.mark.parametrize("n2", DIRECTIONS)
 def test__rT_adjQ_r_as_poly_fixed_cc_distance(r, a1, c1, n1, a2, c2, n2):
     _test__rT_adjQ_r_as_poly(r, a1, c1, n1, a2, c2, n2)
 
 
-@pytest.mark.parametrize('r', DIRECTIONS[0, :]*RADII[:, None])
-@pytest.mark.parametrize('a1', RADII)
-@pytest.mark.parametrize('c1', RADII)
-@pytest.mark.parametrize('n1', [DIRECTIONS[1]])
-@pytest.mark.parametrize('a2', RADII)
-@pytest.mark.parametrize('c2', RADII)
-@pytest.mark.parametrize('n2', [DIRECTIONS[2]])
+@pytest.mark.parametrize("r", DIRECTIONS[0, :] * RADII[:, None])
+@pytest.mark.parametrize("a1", RADII)
+@pytest.mark.parametrize("c1", RADII)
+@pytest.mark.parametrize("n1", [DIRECTIONS[1]])
+@pytest.mark.parametrize("a2", RADII)
+@pytest.mark.parametrize("c2", RADII)
+@pytest.mark.parametrize("n2", [DIRECTIONS[2]])
 def test__rT_adjQ_r_as_poly_variable_cc_distance(r, a1, c1, n1, a2, c2, n2):
     _test__rT_adjQ_r_as_poly(r, a1, c1, n1, a2, c2, n2)
