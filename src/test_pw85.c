fe0444e96bd24e0417f7d636af1512c28b0e83eb
Sebastien Brisard <sebastien.brisard@m4x.org>
2019-09-13 08:24:33 +0200

    Test of pw85_f_neg

--- src/test_pw85.c
+++ src/test_pw85.c
@@ -8,6 +8,8 @@
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_vector.h>
 
+#include <hdf5_hl.h>
+
 #include <pw85.h>
 
 /* #define TEST_PW85_NUM_DIRECTIONS 12 */
@@ -415,6 +417,74 @@ void pw85_test_add_cholesky_solve_test() {
   g_test_add_data_func_full(path, data2, test_pw85_cholesky_solve_test, g_free);
 }
 
+void test_pw85_read_dataset_double(hid_t const hid, char const *dset_name,
+                                   size_t *size, double **buffer) {
+  int ndims;
+  H5LTget_dataset_ndims(hid, dset_name, &ndims);
+  hsize_t *dim = g_new(hsize_t, ndims);
+  H5LTget_dataset_info(hid, dset_name, dim, NULL, NULL);
+  *size = 1;
+  for (size_t i = 0; i < ndims; i++) {
+    *size *= dim[i];
+  }
+  *buffer = g_new(double, *size);
+  H5LTread_dataset_double(hid, dset_name, *buffer);
+  g_free(dim);
+}
+
+void test_pw85_f_neg_test() {
+  hid_t const hid = H5Fopen("../pw85_ref_data.h5", H5F_ACC_RDONLY, H5P_DEFAULT);
+
+  size_t num_directions;
+  double *directions;
+  test_pw85_read_dataset_double(hid, "/directions", &num_directions,
+                                &directions);
+  num_directions /= PW85_DIM;
+
+  size_t num_lambdas;
+  double *lambdas;
+  test_pw85_read_dataset_double(hid, "/lambdas", &num_lambdas, &lambdas);
+
+  size_t num_radii;
+  double *radii;
+  test_pw85_read_dataset_double(hid, "/radii", &num_radii, &radii);
+
+  size_t num_spheroids;
+  double *spheroids;
+  test_pw85_read_dataset_double(hid, "/spheroids", &num_spheroids, &spheroids);
+  num_spheroids /= PW85_SYM;
+
+  size_t num_expecteds;
+  double *expecteds;
+  test_pw85_read_dataset_double(hid, "/F", &num_expecteds, &expecteds);
+
+  double *exp = expecteds;
+  double params[2 * PW85_SYM + PW85_DIM];
+  double rtol = 1e-10;
+  for (size_t i1 = 0; i1 < num_spheroids; i1++) {
+    memcpy(params + PW85_DIM, spheroids + PW85_SYM * i1,
+           PW85_SYM * sizeof(double));
+    for (size_t i2 = 0; i2 < num_spheroids; i2++) {
+      memcpy(params + PW85_DIM + PW85_SYM, spheroids + PW85_SYM * i2,
+             PW85_SYM * sizeof(double));
+      for (size_t i = 0; i < num_directions; i++) {
+        memcpy(params, directions + PW85_DIM * i, PW85_DIM * sizeof(double));
+        for (size_t j = 0; j < num_lambdas; j++, exp++) {
+          double const act = -pw85_f_neg(lambdas[j], params);
+	  g_assert_cmpfloat(fabs(act-*exp), <=, rtol*(*exp));
+        }
+      }
+    }
+  }
+
+  g_free(spheroids);
+  g_free(radii);
+  g_free(lambdas);
+  g_free(directions);
+  H5Fclose(hid);
+}
+
+
 int main(int argc, char **argv) {
   g_test_init(&argc, &argv, NULL);
 
@@ -491,6 +561,8 @@ int main(int argc, char **argv) {
 
   pw85_test_add_cholesky_decomp_test();
   pw85_test_add_cholesky_solve_test();
+
+  g_test_add_func("/pw85/f_neg", test_pw85_f_neg_test);
 
   return g_test_run();
 }
